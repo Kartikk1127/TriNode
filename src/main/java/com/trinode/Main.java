660825ce21c4446b42e1ba7a710c54e7a0d01390
Kartikey Srivastava <kartikey.srivastava1271@gmail.com>
2026-02-24 00:01:12 +0530

    testing

--- src/main/java/com/trinode/Main.java
+++ src/main/java/com/trinode/Main.java
@@ -1,12 +1,33 @@
 package com.trinode;
 
 import com.trinode.config.NodeConfig;
+import com.trinode.core.Node;
+import com.trinode.storage.DataStore;
+
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         NodeConfig config = new NodeConfig();
-        System.out.println("Node count: " + config.getNodeCount());
-        System.out.println("Node 1 address: " + config.getNodeAddress(1));
-        System.out.println("All nodes : " + config.getAllNodes());
+        Map<Integer, String > allNodes = config.getAllNodes();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+
+        node1.start();
+        node2.start();
+        node3.start();
+
+        DataStore ds = node1.getData();
+        ds.put("kartikey","started the project");
+        System.out.println(ds.size());
+        String value = ds.get("kartikey");
+        System.out.println(value);
+        ds.delete("kartikey");
+        System.out.println(ds.size());
+
+        node1.stop();
+        node2.stop();
+        node3.stop();
     }
 }
